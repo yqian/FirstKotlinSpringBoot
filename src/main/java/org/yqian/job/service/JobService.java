84b24865bc83c88f4673b1cfce0728b5a8488c2f
Yi Qian <yi.qian@att.net>
2021-12-14 22:11:06 -0500

    truncate requirement and description to max 16384 Byte and validate it by logging the length

--- src/main/java/org/yqian/job/service/JobService.java
+++ src/main/java/org/yqian/job/service/JobService.java
@@ -1,11 +1,13 @@
 package org.yqian.job.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.yqian.job.entity.JobEntity;
 import org.yqian.job.repository.JobRepository;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class JobService {
 
@@ -16,6 +18,9 @@ public class JobService {
     }
 
     public JobEntity postJob(JobEntity job) {
+        job.setRequirement(job.getRequirement().substring(0, Math.min(job.getRequirement().length(), 16384)));
+        job.setDescription(job.getDescription().substring(0, Math.min(job.getDescription().length(), 16384)));
+        log.info("Job description length is " + job.getRequirement().length());
         return jobRepository.save(job);
     }
 
