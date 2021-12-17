a586a6673ac6ebef601463690b64e78762e11398
Yi Qian <yi.qian@att.net>
2021-12-16 21:12:49 -0500

    Spring Boot main

---
+++ src/main/java/org/yqian/job/JobApplication.java
@@ -0,0 +1,13 @@
+package org.yqian.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class JobApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JobApplication.class);
+    }
+}
